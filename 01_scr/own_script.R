348739f07d2cfac94fc6666fd63624aa22f8214d
Wilhelm Hartung <willi@MacBook-Pro-von-Wilhelm.local>
2025-10-10 16:15:46 +0200

    Updated this file

--- 01_scr/own_script.R
+++ 01_scr/own_script.R
@@ -1,2 +1,25 @@
-if (!require("pacman", quietly = TRUE)) install.packages("pacman")
-pacman::p_load(openxlsx)
+install.packages("pacman")
+install.packages("data.table")
+install.packages("DT")
+
+require(openxlsx)
+require(pacman)
+require(data.table)
+
+fileData <- "/Users/willi/Library/Mobile Documents/com~apple~CloudDocs/Universität/Master/3. Semester/RP Data Science and Machine Learning/Seminar-DS-ML/02_data/Unctad.xlsx"
+
+dataInwardFlow <- read.xlsx(fileData, sheet = 'Inward_Flow')
+dataGDP <- read.xlsx(fileData, sheet = 'GDP')
+
+#data table
+dataInwardFlow <- setDT(dataInwardFlow)
+dataGDP <- setDT(dataGDP)
+
+head(dataGDP, 5)
+
+rm(list = ls())
+
+ls()              # Listet alle Objekte im Environment
+objects()         # Gleiches wie ls()
+str(data)         # Zeigt Struktur einzelner Objekte
+
